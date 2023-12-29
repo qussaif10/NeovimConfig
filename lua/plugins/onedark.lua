6e7cfb45af17d39e4da67ebfd6371c94ad8a5bf1
qussaif10 <qussaif100+1@gmail.com>
2023-12-29 17:04:13 +0300

    initial commit

---
+++ lua/plugins/onedark.lua
@@ -0,0 +1,12 @@
+return {
+	"navarasu/onedark.nvim",
+	lazy = false,
+	name = "onedark",
+	priority = 1000,
+	config = function()
+		require("onedark").setup({
+			style = "cool",
+		})
+		require("onedark").load()
+	end,
+}
